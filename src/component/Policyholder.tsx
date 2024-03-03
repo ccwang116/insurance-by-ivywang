40a13201348b7f3dab1bafeacb45a1aa37b4e8fb
Chao Chun Wang <i22062@hotmail.com>
2024-03-03 22:50:05 +0800

    Feature: Left tree

--- src/component/Policyholder.tsx
+++ src/component/Policyholder.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -74,81 +74,92 @@ const Policyholder = () => {
             style={{ stroke: "gray", strokeWidth: "2px" }}
           />
         </svg>
-        <div style={{ position: "absolute", left: 190 }}>
-          <CustomPaper variant="outlined">00002</CustomPaper>
-        </div>
-
-        <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="122">
-          <line
-            x1="460"
-            y1="62"
-            x2="460"
-            y2="92"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="240"
-            y1="92"
-            x2="740"
-            y2="92"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="240"
-            y1="92"
-            x2="240"
-            y2="122"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="740"
-            y1="92"
-            x2="740"
-            y2="122"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-        </svg>
-        <div style={{ position: "absolute", left: 110 }}>
-          <CustomPaper variant="outlined">00003</CustomPaper>
-        </div>
-        <div style={{ position: "absolute", left: 110 + 120 }}>
-          <CustomPaper variant="outlined">00003</CustomPaper>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="122">
-          <line
-            x1="460"
-            y1="62"
-            x2="460"
-            y2="92"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="240"
-            y1="92"
-            x2="740"
-            y2="92"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="240"
-            y1="92"
-            x2="240"
-            y2="122"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-          <line
-            x1="740"
-            y1="92"
-            x2="740"
-            y2="122"
-            style={{ stroke: "gray", strokeWidth: "2px" }}
-          />
-        </svg>
-        <div style={{ position: "absolute", left: 30 }}>
-          <CustomPaper variant="outlined">00004</CustomPaper>
-        </div>
-        <div style={{ position: "absolute", left: 30 + 120 }}>
-          <CustomPaper variant="outlined">00004</CustomPaper>
+        <div style={{ position: "relative" }}>
+          {[1, 2, 3, 4, 5, 6, 7].map((_, i) => {
+            const layer = Math.floor(Math.log2(i + 1));
+            const eachCount = i + 1 - Math.pow(2, layer);
+            console.log(`i=${i},layer=${layer},eachcount=${eachCount}`);
+            return (
+              <Fragment key={i}>
+                {layer !== 0 && eachCount === 0 && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1000"
+                    height="122"
+                  >
+                    <line
+                      x1={290 + -80 * layer}
+                      y1="62"
+                      x2={290 + -80 * layer}
+                      y2="92"
+                      style={{ stroke: "gray", strokeWidth: "2px" }}
+                    />
+                    <line
+                      x1={220 + -80 * layer}
+                      y1="92"
+                      x2={220 + 240 / layer + -80 * layer}
+                      y2="92"
+                      style={{ stroke: "gray", strokeWidth: "2px" }}
+                    />
+                    <line
+                      x1={220 + -80 * layer}
+                      y1="92"
+                      x2={220 + -80 * layer}
+                      y2="122"
+                      style={{ stroke: "gray", strokeWidth: "2px" }}
+                    />
+                    <line
+                      x1={220 + 240 / layer + -80 * layer}
+                      y1="92"
+                      x2={220 + 240 / layer + -80 * layer}
+                      y2="122"
+                      style={{ stroke: "gray", strokeWidth: "2px" }}
+                    />
+                    {layer === 2 && (
+                      <>
+                        <line
+                          x1={220 + -80 * layer + 310}
+                          y1="62"
+                          x2={220 + -80 * layer + 310}
+                          y2="92"
+                          style={{ stroke: "gray", strokeWidth: "2px" }}
+                        />
+                        <line
+                          x1={220 + -80 * layer + 240}
+                          y1="92"
+                          x2={340 + -80 * layer + 240}
+                          y2="92"
+                          style={{ stroke: "gray", strokeWidth: "2px" }}
+                        />
+                        <line
+                          x1={220 + -80 * layer + 240}
+                          y1="92"
+                          x2={220 + -80 * layer + 240}
+                          y2="122"
+                          style={{ stroke: "gray", strokeWidth: "2px" }}
+                        />
+                        <line
+                          x1={340 + -80 * layer + 240}
+                          y1="92"
+                          x2={340 + -80 * layer + 240}
+                          y2="122"
+                          style={{ stroke: "gray", strokeWidth: "2px" }}
+                        />
+                      </>
+                    )}
+                  </svg>
+                )}
+                <div
+                  style={{
+                    position: "absolute",
+                    left: 190 + -80 * layer + (240 / layer) * eachCount + "px",
+                  }}
+                >
+                  <CustomPaper variant="outlined">00002</CustomPaper>
+                </div>
+              </Fragment>
+            );
+          })}
         </div>
       </Box>
     </>
